5559f4ccff02f558c09e94b53481666cfcc54a3e
Tony DiCola <tony@tonydicola.com>
2016-04-14 20:29:35 +0000

    Whitespace tabs to spaces.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -39,7 +39,7 @@ sensor = BMP085.BMP085()
 # Optionally you can override the bus number:
 #sensor = BMP085.BMP085(busnum=2)
 
-# You can also optionally change the BMP085 mode to one of BMP085_ULTRALOWPOWER, 
+# You can also optionally change the BMP085 mode to one of BMP085_ULTRALOWPOWER,
 # BMP085_STANDARD, BMP085_HIGHRES, or BMP085_ULTRAHIGHRES.  See the BMP085
 # datasheet for more details on the meanings of each mode (accuracy and power
 # consumption are primarily the differences).  The default mode is STANDARD.
--- setup.py
+++ setup.py
@@ -2,13 +2,23 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
-setup(name 				= 'Adafruit_BMP',
-	  version 			= '1.5.0',
-	  author			= 'Tony DiCola',
-	  author_email		= 'tdicola@adafruit.com',
-	  description		= 'Library for accessing the BMP series pressure and temperature sensors like the BMP085/BMP180 on a Raspberry Pi or Beaglebone Black.',
-	  license			= 'MIT',
-	  url				= 'https://github.com/adafruit/Adafruit_Python_BMP/',
-	  dependency_links	= ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
-	  install_requires	= ['Adafruit-GPIO>=0.6.5'],
-	  packages 			= find_packages())
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name              = 'Adafruit_BMP',
+      version           = '1.5.1',
+      author            = 'Tony DiCola',
+      author_email      = 'tdicola@adafruit.com',
+      description       = 'Library for accessing the BMP series pressure and temperature sensors like the BMP085/BMP180 on a Raspberry Pi or Beaglebone Black.',
+      license           = 'MIT',
+      classifiers       = classifiers,
+      url               = 'https://github.com/adafruit/Adafruit_Python_BMP/',
+      dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
+      install_requires  = ['Adafruit-GPIO>=0.6.5'],
+      packages          = find_packages())
